7a25f005869d4f6444c8174f0ac62aaea05ee2f2
Naoto Tsushima <shogi2nao28@gmail.com>
2025-04-07 21:45:29 +0900

    Add types

--- parallel_download.py
+++ parallel_download.py
@@ -1,9 +1,9 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor, as_completed, Future
 import requests
 
-def download_file(url):
+def download_file(url) -> bytes | None:
     try:
-        response = requests.get(url)
+        response: requests.Response = requests.get(url)
         response.raise_for_status()  # エラーが発生した場合に例外を発生させる
         return response.content
     except requests.exceptions.RequestException as e:
@@ -17,6 +17,10 @@ urls = [
 ]
 
 with ThreadPoolExecutor(max_workers=3) as executor:
-    futures = [executor.submit(download_file, url) for url in urls]
+    futures: list[Future[bytes | None]] = [executor.submit(download_file, url) for url in urls]
     for future in as_completed(futures):
-        print(future.result())
+        result: bytes | None = future.result()
+        if result:
+            print(result)
+        else:
+            print("Failed to download")
